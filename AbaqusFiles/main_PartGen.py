31aaf757f3a41de765e97cff862993adee6f9bea
WeianJiang <tbszjwa@outlook.com>
2019-08-09 23:11:29 +0800

    update

--- AbaqusFiles/main_PartGen.py
+++ AbaqusFiles/main_PartGen.py
@@ -29,3 +29,17 @@ def partCircleGen(partname,target_x,target_y,radi):#single CIrcle
     s1.unsetPrimaryObject()
     p = mdb.models['Model-1'].parts[partname]
     del mdb.models['Model-1'].sketches['__profile__']
+
+
+def interfaceGen(partname,target_x,target_y,outterRadi,innerRadi):#not finished
+    s1 = mdb.models['Model-1'].ConstrainedSketch(name='__profile__', sheetSize=150.0)
+    g, v, d, c = s1.geometry, s1.vertices, s1.dimensions, s1.constraints
+    s1.setPrimaryObject(option=STANDALONE)
+    s1.CircleByCenterPerimeter(center=(target_x,target_y), point1=(target_x+radi, target_y))#setCircle
+    p = mdb.models['Model-1'].Part(name=partname, dimensionality=TWO_D_PLANAR, 
+    type=DEFORMABLE_BODY)
+    p = mdb.models['Model-1'].parts[partname]
+    p.BaseShell(sketch=s1)
+    s1.unsetPrimaryObject()
+    p = mdb.models['Model-1'].parts[partname]
+    del mdb.models['Model-1'].sketches['__profile__']
--- main.py
+++ main.py
@@ -11,32 +11,34 @@ import numpy as np
 
 
 circleData = np.loadtxt('Circle.txt')
-ringData=np.loadtxt('ringData.txt')
+interfaceData=np.loadtxt('ringData.txt')#load the interface data
 innerCircleData=np.loadtxt('innerCircleData.txt')
-partName=[]
-for number in range(len(circleData)):
-    partName.append('Part-'+str(number))
+CoarseAggregate=[]
+interface=[]
+partNumbers=len(circleData)
+for number in range(partNumbers):
+    CoarseAggregate.append('CoarseAggregate-'+str(number))
+    interface.append('interface-'+str(number))
 #part generating----------------------------------------
-main_PartGen.partRectGen('MainPart',innerCircleData)
+main_PartGen.partRectGen('MainPart',circleData)#generating the retangle
 GraniteElastic=np.loadtxt('GraniteElastic.txt')
 
-partNumbers=len(partName)
 
 for number in range(partNumbers):
-    main_PartGen.partCircleGen(partName[number],circleData[number][0],circleData[number][1],circleData[number][2])
-    main_Property.materialCreate(partName[number],GraniteElastic[number],0.3)#property of rock
-    main_Property.sectionCreate(partName[number],partName[number])
-    main_Property.assignSection(partName[number],partName[number])
+    main_PartGen.partCircleGen(CoarseAggregate[number],circleData[number][0],circleData[number][1],circleData[number][2])
+    main_Property.materialCreate(CoarseAggregate[number],GraniteElastic[number],0.3)#property of rock
+    main_Property.sectionCreate(CoarseAggregate[number],CoarseAggregate[number])
+    main_Property.assignSection(CoarseAggregate[number],CoarseAggregate[number])
 
 main_Property.materialCreate('MainPart',134000,0.3)#property of mortar
 main_Property.sectionCreate('MainPart','MainPart')
 main_Property.assignSection('MainPart','MainPart')
 
 main_PartAssem.partInst('MainPart')
-map(main_PartAssem.partInst,partName)
+map(main_PartAssem.partInst,CoarseAggregate)
 
 for number in range(partNumbers):
-    main_Interaction.creatingTie('MainPart',partName[number],circleData[number][0],circleData[number][1],circleData[number][2],number)
+    main_Interaction.creatingTie('MainPart',CoarseAggregate[number],circleData[number][0],circleData[number][1],circleData[number][2],number)
 
 
 #-----------------------------step
@@ -48,4 +50,4 @@ main_Load.setBoundary('MainPart',1)
 main_Mesh.Mesh('MainPart',14)
 
 for number in range(partNumbers):
-    main_Mesh.Mesh(partName[number],2)
+    main_Mesh.Mesh(CoarseAggregate[number],2)
