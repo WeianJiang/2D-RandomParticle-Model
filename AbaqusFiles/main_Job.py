7b7a048ff7fbfc13694a8f385e88cd674651df6d
WeianJiang <tbszjwa@outlook.com>
2019-08-28 13:30:08 +0800

    Update main_Job.py

--- AbaqusFiles/main_Job.py
+++ AbaqusFiles/main_Job.py
@@ -4,7 +4,7 @@ from abaqusConstants import *
 import job
 
 def creatJob(name,cpus=1):
-    mdb.Job(name='Job-1', model='Model-1', description='', type=ANALYSIS, 
+    mdb.Job(name=name, model='Model-1', description='', type=ANALYSIS, 
         atTime=None, waitMinutes=0, waitHours=0, queue=None, memory=90, 
         memoryUnits=PERCENTAGE, getMemoryFromAnalysis=True, 
         explicitPrecision=SINGLE, nodalOutputPrecision=SINGLE, echoPrint=OFF, 
