2135e1ebba18fcffbe6d097317a31f58c1adb962
WeianJiang <tbszjwa@outlook.com>
2019-09-03 10:58:02 +0800

    Update main_Property.py

--- AbaqusFiles/main_Property.py
+++ AbaqusFiles/main_Property.py
@@ -22,15 +22,12 @@ def assignSection(partName,sectionName):
         thicknessAssignment=FROM_SECTION)
 
 def PLassign(materialName):
+        import numpy as np
+        Compress=np.loadtxt('Compression.txt')
+        Tensile=np.loadtxt('Tension.txt')
         mdb.models['Model-1'].materials[materialName].ConcreteDamagedPlasticity(table=((
         38.0, 0.1, 1.16, 0.667, 0.0), ))
         mdb.models['Model-1'].materials[materialName].concreteDamagedPlasticity.ConcreteCompressionHardening(
-        table=((14.07, 0.0), (20.1, 0.000801898), (14.6366, 0.00245591), (10.0733, 
-        0.00407992), (7.50085, 0.00563756), (5.93113, 0.00716179), (4.88986, 
-        0.00866839), (4.15349, 0.0101648), (3.607, 0.011655), (3.18609, 
-        0.0131409)))
+        table=(Compress))
         mdb.models['Model-1'].materials[materialName].concreteDamagedPlasticity.ConcreteTensionStiffening(
-        table=((2.0301, 0.0), (2.01, 2.82563e-05), (1.23219, 0.00014944), (
-        0.849073, 0.000257466), (0.660524, 0.000359008), (0.548371, 0.000458002), (
-        0.473404, 0.000555757), (0.419357, 0.000652815), (0.378298, 0.00074944), (
-        0.345892, 0.000845777), (0.118271, 0.00380631)))
+        table=(Tensile))
