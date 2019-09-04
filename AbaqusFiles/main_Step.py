8ebef4dff371ff80107a4ab2a60c514648ae07b3
WeianJiang <tbszjwa@outlook.com>
2019-09-04 15:46:27 +0800

    Update main_Step.py

--- AbaqusFiles/main_Step.py
+++ AbaqusFiles/main_Step.py
@@ -2,4 +2,8 @@ from abaqus import *
 from abaqusConstants import *
 
 def createStep(stepName,previousStep):
-    mdb.models['Model-1'].StaticStep(name=stepName, previous=previousStep)
+    #mdb.models['Model-1'].StaticStep(name=stepName, previous=previousStep)
+    #mdb.models['Model-1'].StaticStep(name=stepName, previous=previousStep)
+    mdb.models['Model-1'].ImplicitDynamicsStep(name=stepName, previous=previousStep, 
+    timePeriod=2.0, maxNumInc=100000, initialInc=5e-05, minInc=1e-015, 
+    maxInc=0.01)
