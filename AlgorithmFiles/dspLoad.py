70e643d4de51ca2ee4c621edaf3cc04bc313397f
WeianJiang <tbszjwa@outlook.com>
2019-09-05 22:05:16 +0800

    encapsulation

---
+++ AlgorithmFiles/dspLoad.py
@@ -0,0 +1,13 @@
+from AbaqusFiles import main_PartGen
+from AbaqusFiles import main_Property
+from AbaqusFiles import main_PartAssem
+from AbaqusFiles import main_Interaction
+from AbaqusFiles import main_Load
+from AbaqusFiles import main_Mesh
+from AbaqusFiles import main_Step
+from AbaqusFiles import main_Job
+
+class dspLoad():
+
+    def importDataFromFile(self,FileName):
+        
