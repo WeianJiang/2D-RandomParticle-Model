8477bdf3344855b9a1afd577d33cbbfbf381bb85
WeianJiang <tbszjwa@outlook.com>
2019-09-06 10:49:08 +0800

    Update dspLoad.py

--- AlgorithmFiles/dspLoad.py
+++ AlgorithmFiles/dspLoad.py
@@ -9,5 +9,20 @@ from AbaqusFiles import main_Job
 
 class dspLoad():
 
-    def importDataFromFile(self,FileName):
+    def setImportFile(self,CircleFileName='Circle.txt',InterFaceFileName='ringData.txt',innerCircleFileName='innerCircleData.txt'):
+        import numpy as np
+        self.circleData = np.loadtxt(CircleFileName)
+        self.interfaceData=np.loadtxt(InterFaceFileName)#load the interface data
+        self.innerCircleData=np.loadtxt(innerCircleFileName)
+        self.CoarseAggregate=[]
+        self.interface=[]
+        self.partNumbers=len(self.circleData)
+        for number in range(self.partNumbers):
+            self.CoarseAggregate.append('CoarseAggregate-'+str(number))
+            self.interface.append('interface-'+str(number))
+
+    
+    def setImportMaterial(self,GraniteElastic,InterfaceElastic):
         
+    
+    def __CreatePart():
